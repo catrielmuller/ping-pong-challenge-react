cdd9ffdaab8faf597c71060dbc01792fe5d586f3
Catriel Müller <catrielmuller@gmail.com>
2020-02-16 16:21:17 -0300

    Updates

---
+++ precache-manifest.b4951466e1cd870aaccd26f2da0ff797.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea210447633d949926066dad6aca9345",
+    "url": "/ping-pong-challenge-react/index.html"
+  },
+  {
+    "revision": "50c3586209079f5f8e49",
+    "url": "/ping-pong-challenge-react/static/css/main.6fa756ed.chunk.css"
+  },
+  {
+    "revision": "35173e8aa457141b0fd0",
+    "url": "/ping-pong-challenge-react/static/js/2.1d440e44.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ping-pong-challenge-react/static/js/2.1d440e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c3586209079f5f8e49",
+    "url": "/ping-pong-challenge-react/static/js/main.1c36f828.chunk.js"
+  },
+  {
+    "revision": "9dc5d60a7e2484765950",
+    "url": "/ping-pong-challenge-react/static/js/runtime-main.1150cba4.js"
+  },
+  {
+    "revision": "09c537af05aa13c526edd6f575ba70a1",
+    "url": "/ping-pong-challenge-react/static/media/ball-incoming.09c537af.mp3"
+  },
+  {
+    "revision": "177649cab3350934ff5ebb558e8dfe35",
+    "url": "/ping-pong-challenge-react/static/media/ball-outgoing.177649ca.mp3"
+  },
+  {
+    "revision": "f945164ca74fa34ca11838eae0995494",
+    "url": "/ping-pong-challenge-react/static/media/game-over.f945164c.mp3"
+  },
+  {
+    "revision": "1395a2aa2f85a0f5d64fadf1321d52e1",
+    "url": "/ping-pong-challenge-react/static/media/loop.1395a2aa.mp3"
+  }
+]);
